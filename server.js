61f59dc8644d622de7d7f79f81078922c888b657
sphen <sphen@users.noreply.github.com>
2021-02-25 15:10:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
 app.listen(
